43a44e10b3a533bbd747971f541da7481218b657
Rushi Patel <rp970.patel@gmail.com>
2022-06-30 01:57:11 +0930

    build(aws-sdk-v2-typescript-webpack): update config

--- examples/aws-sdk-v2-typescript-webpack/webpack.config.js
+++ examples/aws-sdk-v2-typescript-webpack/webpack.config.js
@@ -8,26 +8,32 @@ export default {
   entry: [path.resolve('../../dist/examples/aws-sdk-v2-typescript-webpack')],
   // Specify the output file containing our bundled code
   output: {
-    path: path.resolve('../../dist/examples/aws-sdk-v2-typescript-webpack/bundle'),
+    path: path.resolve(
+      '../../dist/examples/aws-sdk-v2-typescript-webpack/bundle'
+    ),
     filename: 'index.js',
   },
   module: {
-    rules: [{
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false
-      }
-    }]
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
   },
   target: 'node16',
   devtool: 'source-map',
   mode: 'production',
-  externals: [{
-    ['@aws-sdk/client-dynamodb']: {
-      root: '@aws-sdk/client-dynamodb'
+  externals: [
+    {
+      ['@aws-sdk/client-dynamodb']: {
+        root: '@aws-sdk/client-dynamodb',
+      },
+      ['@aws-sdk/lib-dynamodb']: {
+        root: '@aws-sdk/lib-dynamodb',
+      },
     },
-    ['@aws-sdk/lib-dynamodb']: {
-      root: '@aws-sdk/lib-dynamodb'
-    }
-  }],
+  ],
 };
